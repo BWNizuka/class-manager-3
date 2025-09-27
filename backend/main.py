24d1d4d3883366e8078898545e5c509cfa5f2e80
BWNizuka <burningwings.nth@gmail.com>
2025-09-27 09:55:57 +0700

    Update and rename backend/main.py to backend/backend/app.py

--- backend/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from backend.controllers import student_controller, class_controller
-
-app = FastAPI()
-
-app.include_router(student_controller.router, prefix="/students", tags=["Students"])
-app.include_router(class_controller.router, prefix="/classes", tags=["Classes"])
