895d174176cd5550b4960aac78bea8150b1192a1
BWNizuka <burningwings.nth@gmail.com>
2025-09-27 09:37:27 +0700

    Update class.py

--- backend/models/class.py
+++ backend/models/class.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 from .student import Student
+from .teacher import Teacher
 
 class Class(BaseModel):
     id: int
     name: str
     students: List[Student] = []
+    teacher: Optional[Teacher] = None
