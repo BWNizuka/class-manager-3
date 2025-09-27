3c0196021e375653453f0adef0ee24552ac1475e
BWNizuka <burningwings.nth@gmail.com>
2025-09-27 09:55:03 +0700

    Delete backend/models directory

--- backend/models/student.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class Student(BaseModel):
-    id: int
-    name: str
-    grade: str
-    email: str
--- backend/models/teacher.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class Teacher(BaseModel):
-    id: int
-    name: str
-    subject: str
-    email: str
