178886d30005430e7c582549f0f00a78d77a6b8f
BWNizuka <burningwings.nth@gmail.com>
2025-09-27 10:11:54 +0700

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,17 +1,12 @@
 # frontend/app.py
 import streamlit as st
 import pandas as pd
-from backend.logic import manager, Student, Teacher, Course, db
 import sys
 import os
+from backend.logic import manager, Student, Teacher, Course, db 
 
 # Thêm thư mục cha (classmanager) vào sys.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-import streamlit as st
-import pandas as pd
-from backend.logic import manager, Student, Teacher, Course, db
-
 
 # -----------------------------
 # Streamlit UI
